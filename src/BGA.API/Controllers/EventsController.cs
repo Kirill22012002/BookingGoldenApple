5b6421157eb65a1992b4981770bab809ab26493e
Kirill <perepechkin_2002@mail.ru>
2026-03-04 10:14:00 +0300

    add Swagger support and Route for EventsController

--- src/BGA.API/Controllers/EventsController.cs
+++ src/BGA.API/Controllers/EventsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BGA.API.Controllers;
 
 [ApiController]
+[Route("[controller]")]
 public class EventsController : ControllerBase
 {
     
--- src/BGA.API/Program.cs
+++ src/BGA.API/Program.cs
@@ -4,13 +4,20 @@ using BGA.API.Services.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<IEventService, EventService>();
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
 app.UseRouting();
 app.MapControllers();
-
 app.Run();
