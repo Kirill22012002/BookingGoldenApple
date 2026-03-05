5317ff474a123e8f636ea909d0500d17ff7d259e
Kirill <perepechkin_2002@mail.ru>
2026-03-05 10:49:09 +0300

    add increment eventId when add to list

--- src/BGA.API/Infrastructure/Repositories/EventRepository.cs
+++ src/BGA.API/Infrastructure/Repositories/EventRepository.cs
@@ -2,4 +2,18 @@ using BGA.API.Infrastructure.Models;
 
 namespace BGA.API.Infrastructure.Repositories;
 
-public class EventRepository : List<Event> { }
+public class EventRepository : List<Event>
+{
+    private int _eventId = 0;
+
+    public new void Add(Event item)
+    {
+        if (item.Id <= 0)
+        {
+            Interlocked.Increment(ref _eventId);
+            item.Id = _eventId;
+        }
+
+        base.Add(item);
+    }
+}
