d0adc48961e640644e0179b35c81501bf9e5c6b6
Kirill <perepechkin_2002@mail.ru>
2026-03-07 09:00:27 +0300

    add IsValid override for FieldRequiredAttribute

--- src/BGA.API/Presentation/Attributes/FieldRequiredAttribute.cs
+++ src/BGA.API/Presentation/Attributes/FieldRequiredAttribute.cs
@@ -2,12 +2,32 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BGA.API.Presentation.Attributes;
 
-public sealed class FieldRequiredAttribute : RequiredAttribute
+public sealed class FieldRequiredAttribute : ValidationAttribute
 {
     public override string FormatErrorMessage(string fieldName)
     {
         return !string.IsNullOrEmpty(ErrorMessage)
             ? ErrorMessage
             : $"The {fieldName} field is required";
+    }
+
+    public override bool IsValid(object? value)
+    {
+        if (value == null)
+        {
+            return false;
+        }
+
+        Type type = value.GetType();
+        if (type.IsValueType)
+        {
+            var defaultValue = Activator.CreateInstance(type);
+            if (Equals(value, defaultValue))
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
