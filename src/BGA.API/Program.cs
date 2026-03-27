6ab835da9e0dfcf911142de72c0910a1b011284a
Kirill <perepechkin_2002@mail.ru>
2026-03-27 11:54:09 +0300

    add invoking all validation errors and make it to problemDetails

--- src/BGA.API/Program.cs
+++ src/BGA.API/Program.cs
@@ -3,6 +3,7 @@ using BGA.API.Application.Services.Interfaces;
 using BGA.API.Presentation;
 using BGA.API.Infrastructure.Repositories.Interfaces;
 using BGA.API.Infrastructure.Repositories.Implementations;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +17,28 @@ builder.Services.AddProblemDetails(options =>
     };
 });
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var errors = context.ModelState
+                .Where(kv => kv.Value?.Errors.Count > 0)
+                .ToDictionary(
+                    kv => kv.Key,
+                    kv => kv.Value!.Errors.Select(e => e.ErrorMessage));
+
+            var problemDetails = new ValidationProblemDetails(context.ModelState)
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Title = "Request Validation Errors",
+                Detail = string.Join(".", errors.Select(kv => $"{kv.Key}: {kv.Value}"))
+            };
+
+            return new BadRequestObjectResult(problemDetails);
+        };
+    });
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
